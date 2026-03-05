3f9525721a25bbc685c0ab286ce723df45d90320
IPandragonI <mathysf84pro@gmail.com>
2026-03-05 18:27:28 +0100

    Add initial configuration files and API tests for user authentication

--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 const datasource = new DataSource({
   type: "postgres",
-  host: "postgres",
-  username: "postgres",
-  password: "supersecret",
-  database: "favorite_places",
-  port: 5432,
+  host: process.env.DB_HOST || "postgres",
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "supersecret",
+  database: process.env.DB_NAME || "favorite_places",
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
   entities: [__dirname + "/entities/**/*.{js,ts}"],
   logging: true,
   synchronize: true,
